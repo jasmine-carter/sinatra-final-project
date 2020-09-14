e75d4f9c1dedba65e4eb384bbebde5dd2bdbbb3b
Jasmine Carter <heyjasminec@gmail.com>
2020-09-14 09:16:05 -0400

    Create helper methods for user session management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,15 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||=User.find_by(id: session[:user_id])
+    end
+  end
 end
